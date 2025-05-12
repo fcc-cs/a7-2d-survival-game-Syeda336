c79e46e55709dba2f5cb0f5d01d53db6e5923718
Syeda336 <116817726+Syeda336@users.noreply.github.com>
2025-05-12 13:25:23 +0500

    Player Movement Script (First Video Done)

---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var SPEED = 100.0
+var player_state
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 and direction.y != 0:
+		player_state = "walking"
+	
+	velocity = direction * SPEED
+	move_and_slide()
+	
+	player_anim(direction)
+
+func player_anim(dir):
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	if player_state == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("north_walk")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("east_walk")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("south_walk")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("west_walk")
+			
+		if dir.x > 0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("north_east_walk")
+		if dir.x > 0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("south_east_walk")
+		if dir.x < -0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("south_west_walk")
+		if dir.x < -0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("north_west_walk")
+
+func player():
+	pass
+	
