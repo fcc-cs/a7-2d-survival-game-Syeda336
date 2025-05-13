40ee380ef12fa76e9700765343900d68a545cc21
Syeda336 <116817726+Syeda336@users.noreply.github.com>
2025-05-13 13:42:30 +0500

    Update apple_tree.gd

--- scripts/apple_tree.gd
+++ scripts/apple_tree.gd
@@ -3,6 +3,8 @@ extends Node2D
 var state = "no apples"
 var player_in_area = false
 
+var apple = preload("res://scenes/apple_collectable.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if state == "no apples":
@@ -18,7 +20,7 @@ func _process(delta: float) -> void:
 		if player_in_area:
 			if Input.is_action_just_pressed("mouse_left"):
 				state = "no apples"
-				$growth_timer.start()
+				drop_apple()
 
 
 func _on_pickable_area_body_entered(body: Node2D) -> void:
@@ -34,3 +36,11 @@ func _on_pickable_area_body_exited(body: Node2D) -> void:
 func _on_growth_timer_timeout() -> void:
 	if state == "no apples":
 		state = "apples"
+		
+func drop_apple():
+	var apple_instance = apple.instantiate()
+	apple_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(apple_instance)
+	
+	await get_tree().create_timer(3).timeout
+	$growth_timer.start()
